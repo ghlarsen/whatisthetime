41b421e08604aae1fe9c2f4498ce31de4ed0c009
Sebastian Larsen <gh@larsen.studio>
2026-03-26 07:11:34 +0100

    fix: trailingSlash: never (Google seeing duplicate URLs with/without slash)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   output: 'static',
+  trailingSlash: 'never',
   adapter: cloudflare({ imageService: 'compile' }),
   site: 'https://whatisthetime.now',
   vite: {
